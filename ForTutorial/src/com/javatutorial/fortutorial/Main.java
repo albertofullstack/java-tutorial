047a5665084f52972acf556dfded8d49e8732318
amoranteb <amoranteb@minsait.com>
2022-08-24 17:19:32 +0200

    Implements for tutorial.

---
+++ ForTutorial/src/com/javatutorial/fortutorial/Main.java
@@ -0,0 +1,11 @@
+package com.javatutorial.fortutorial;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    for (int i = 0; i < 5; i++) {
+      System.out.printf("The value of i is: %d\n", i);
+    }
+  }
+}
