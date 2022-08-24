838cbb73fbf28e0c42f056323b23a84efd1c4bbe
amoranteb <amoranteb@minsait.com>
2022-08-24 17:03:32 +0200

    Implements string tutorial.

---
+++ StringTutorial/src/com/javatutorial/stringtutorial/Main.java
@@ -0,0 +1,26 @@
+package com.javatutorial.stringtutorial;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    int myInt = 7;
+
+    String text = "Hello";
+
+    String blank = " ";
+
+    String name = "Alberto";
+
+    String greeting = text + blank + name;
+
+    System.out.println(greeting);
+
+    System.out.println("Hello" + " " + "Alberto");
+
+    System.out.println("My integer is: " + myInt);
+
+    double myDouble = 7.8;
+    System.out.println("My number is: " + myDouble + ".");
+  }
+}
