0e0db8a42bf3c1a251674e5b77d380d5da4ce03e
amoranteb <amoranteb@minsait.com>
2022-08-24 18:17:31 +0200

    Implements do while tutorial.

---
+++ DoWhileTutorial/src/com/javatutorial/dowhile/Main.java
@@ -0,0 +1,17 @@
+package com.javatutorial.dowhile;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    int value = 0;
+    do {
+      System.out.println("Enter a number: ");
+      value = scanner.nextInt();
+    } while(value != 5);
+    System.out.println("Got 5!");
+  }
+}
