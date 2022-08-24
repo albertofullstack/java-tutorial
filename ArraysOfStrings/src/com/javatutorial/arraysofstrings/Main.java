8c40cd7cd0e81d473eab8ea424a92e79aca94291
amoranteb <amoranteb@minsait.com>
2022-08-24 19:22:18 +0200

    Implements Arrays of Strings tutorial.

---
+++ ArraysOfStrings/src/com/javatutorial/arraysofstrings/Main.java
@@ -0,0 +1,33 @@
+package com.javatutorial.arraysofstrings;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    String[] words = new String[3];
+
+    words[0] = "Hello";
+    words[1] = "to";
+    words[2] = "you";
+
+    System.out.println(words[2]);
+
+    String[] fruits = {"apple", "pineapple", "yellow kiwi"};
+
+    for (String fruit: fruits) {
+      System.out.println(fruit);
+    }
+
+    int value = 123;
+
+    String text = null;
+
+    System.out.println(text);
+
+    String[] texts = new String[2];
+
+    System.out.println(texts[0]);
+
+    texts[0] = "one";
+  }
+}
