9e7088ea252e13baed782903adac10e2d3ca0fde
amoranteb <amoranteb@minsait.com>
2022-08-25 18:07:28 +0200

    Implements setters tutorial.

---
+++ SettersAndThisTutorial/src/com/javatutorial/settersandthis/Main.java
@@ -0,0 +1,41 @@
+package com.javatutorial.settersandthis;
+
+class Frog {
+
+  private String name;
+  private int age;
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setInfo(String name, int age) {
+    setName(name);
+    setAge(age);
+  }
+}
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Frog frog1 = new Frog();
+
+    frog1.setName("Bertie");
+    frog1.setAge(1);
+
+    System.out.println(frog1.getName());
+  }
+}
