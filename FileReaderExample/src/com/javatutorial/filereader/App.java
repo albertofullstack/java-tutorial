810c12f41b943a108754c40f340df4b8c449e6e7
amoranteb <amoranteb@minsait.com>
2022-08-27 19:43:03 +0200

    Implements a file reader.

---
+++ FileReaderExample/src/com/javatutorial/filereader/App.java
@@ -0,0 +1,40 @@
+package com.javatutorial.filereader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class App {
+
+  public static void main(String[] args) {
+
+    File file = new File("test.txt");
+
+    BufferedReader br = null;
+
+    try {
+      FileReader fr = new FileReader(file);
+      br = new BufferedReader(fr);
+
+      String line;
+
+      while ((line = br.readLine()) != null) {
+        System.out.println(line);
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found: " + file);
+    } catch (IOException e) {
+      System.out.println("Unable to read file: " + file);
+    } finally {
+      try {
+        br.close();
+      } catch (IOException e) {
+        System.out.println("Unable to close file: " + file);
+      } catch (NullPointerException ex) {
+        // File was probably never opened
+      }
+    }
+  }
+}
