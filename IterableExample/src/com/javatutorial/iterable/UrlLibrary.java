20b35d826c97705a142bb3fa7edbd622f802d525
amoranteb <amoranteb@minsait.com>
2022-08-31 18:05:45 +0200

    Adds example of iterable.

---
+++ IterableExample/src/com/javatutorial/iterable/UrlLibrary.java
@@ -0,0 +1,66 @@
+package com.javatutorial.iterable;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class UrlLibrary implements Iterable<String>{
+
+  private List<String> urls = new LinkedList<>();
+
+  private class UrlIterator implements Iterator<String> {
+
+    private int index = 0;
+
+    @Override
+    public boolean hasNext() {
+      return index < urls.size();
+    }
+
+    @Override
+    public String next() {
+
+      StringBuilder sb = new StringBuilder();
+
+      try {
+        URL url = new URL(urls.get(index));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+
+        String line = null;
+
+        while((line = br.readLine()) != null) {
+          sb.append(line);
+          sb.append("\n");
+        }
+
+        br.close();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      index++;
+
+      return sb.toString();
+    }
+
+    @Override
+    public void remove() {
+      urls.remove(index);
+    }
+  }
+
+  public UrlLibrary() {
+    urls.add("http://www.wikipedia.com");
+    urls.add("http://www.refactorizando.com");
+    urls.add("http://news.bbc.co.uk");
+  }
+
+  @Override
+  public Iterator<String> iterator() {
+    return new UrlIterator();
+  }
+}
